2b550aa0a71d7831810ca68438c9e93eb594467e
Lars Kellogg-Stedman <lars@oddbit.com>
2017-11-15 09:43:59 -0500

    use iterators for get/put operations

--- example.py
+++ example.py
@@ -2,6 +2,11 @@ import os
 
 from noggin.app import App, Response, HTTPError
 
+# cribbed from
+# https://github.com/micropython/micropython-lib/blob/master/stat/stat.py
+S_IFDIR = 0o040000
+S_IFMT = 0o170000
+
 app = App()
 
 config = {
@@ -14,7 +19,7 @@ helptext = '''<html>
 <h1>About this webapp</h1>
 
 <p>This is a demo webapp. For more information, see
-<a href="https://github.com/larsks/noggin">Noggin</a> on
+<a href="https://github.com/larsks/micropython-noggin">Noggin</a> on
 GitHub.</p>
 
 </html>'''
@@ -40,16 +45,52 @@ def oops(req, match):
     raise HTTPError(418)
 
 
-@app.route('/get/(.*)')
+@app.route('/echo', method='PUT')
+def echo(req, match):
+    return req.content
+
+
+@app.route('/disk')
+def disk_stats(req, match):
+    statvfs_fields = [
+        'sb.f_bsize',
+        'sb.f_frsize',
+        'sb.f_blocks',
+        'sb.f_bfree',
+        'sb.f_bavail',
+        'sb.f_files',
+        'sb.f_ffree',
+    ]
+    return dict(zip(statvfs_fields, os.statvfs('/')))
+
+
+@app.route('/file/(.*)')
 def get_file(req, match):
+    path = match.group(1)
+    print('* request to get {}'.format(path))
+    buf = bytearray(256)
     try:
-        with open(match.group(1)) as fd:
-            return fd.read()
+        with open(path) as fd:
+            while True:
+                nb = fd.readinto(buf)
+                if not nb:
+                    break
+                yield buf[:nb]
     except OSError:
         raise HTTPError(404)
 
 
-@app.route('/put/(.*)', method='PUT')
+@app.route('/file/(.*)', method='DELETE')
+def del_file(req, match):
+    path = match.group(1)
+    print('* request to delete {}'.format(path))
+    try:
+        os.remove(path)
+    except OSError:
+        raise HTTPError(404)
+
+
+@app.route('/file/(.*)', method='PUT')
 def put_file(req, match):
     path = match.group(1)
     print('* request to put {}'.format(path))
@@ -64,6 +105,7 @@ def put_file(req, match):
             pass
 
     with open(path, 'w') as fd:
-        fd.write(req.content)
+        for chunk in req.iter_content():
+            fd.write(chunk)
 
 app.serve()
