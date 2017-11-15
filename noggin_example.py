c8715903b01f059f99a49662ba969e68baca8ce7
Lars Kellogg-Stedman <lars@oddbit.com>
2017-11-15 10:24:08 -0500

    added file listing

--- noggin_example.py
+++ noggin_example.py
@@ -27,31 +27,39 @@ GitHub.</p>
 
 @app.route('/')
 def index(req, match):
-    return 'Hello world!\n'
-
-
-@app.route('/help')
-def get_help(req, match):
     return Response(content=helptext, mimetype='text/html')
 
 
 @app.route('/config')
 def get_config(req, match):
+    '''returning a dictionary (or a list) will send a JSON response
+    to the client.'''
     return config
 
 
 @app.route('/error')
 def oops(req, match):
+    '''raise HTTPError to send http errors to the client.'''
     raise HTTPError(418)
 
 
 @app.route('/echo', methods=['PUT', 'POST'])
 def echo(req, match):
+    '''This will echo content back to the client, but will fail for
+    "large" requests because everything is read into memory.'''
     return req.content
+
+
+@app.route('/yell', methods=['PUT', 'POST'])
+def echo(req, match):
+    '''Like echo, but implemented with memory-efficient iterables so
+    that it should work regardless of the size of the request.'''
+    yield from req.iter_content()
 
 
 @app.route('/disk')
 def disk_stats(req, match):
+    '''Return information about the filesystem.'''
     statvfs_fields = [
         'sb.f_bsize',
         'sb.f_frsize',
@@ -62,6 +70,33 @@ def disk_stats(req, match):
         'sb.f_ffree',
     ]
     return dict(zip(statvfs_fields, os.statvfs('/')))
+
+
+def get_file_list(path):
+    '''Recursively list files.
+
+    Returns a list of (name, size, is_dir, children) tuples, where
+    children is a similar list of is_dir is True or None of is_dir
+    is False.
+    '''
+
+    files = []
+
+    for f in os.listdir(path):
+        fp = '/'.join([path, f])
+        print('* checking', f)
+        s = os.stat(fp)
+        if s[0] & S_IFMT == S_IFDIR:
+            files.append((f, s[6], True, get_file_list(fp)))
+        else:
+            files.append((f, s[6], False, None))
+
+    return files
+
+
+@app.route('/file')
+def list_files(req, match):
+    return get_file_list('/')
 
 
 @app.route('/file/(.*)')
