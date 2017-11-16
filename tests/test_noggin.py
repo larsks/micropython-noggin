cf01b4e2c8157bc63bd40648220cdd94a0dc8e97
Lars Kellogg-Stedman <lars@oddbit.com>
2017-11-16 17:44:46 -0500

    use mock.patch as a class decorator

--- tests/test_noggin.py
+++ tests/test_noggin.py
@@ -4,151 +4,133 @@ from unittest.mock import MagicMock, patch
 import noggin
 
 
+def fake_recv_into(self, buf, buflen=0, flags=0):
+    c = self.recv(1)
+    if c is None:
+        return 0
+
+    buf[0] = ord(c)
+    return 1
+
+
+@patch('noggin.compat.socket.mpsocket.recv_into', fake_recv_into)
+@patch('noggin.compat.socket.mpsocket.send')
+@patch('noggin.compat.socket.mpsocket.recv')
 class TestNoggin(TestCase):
     def setUp(self):
         self.app = noggin.Noggin()
 
-    def test_create_app(self):
+    def test_create_app(self, mock_recv, mock_send):
         assert self.app
 
-    def test_route_simple(self):
+    def test_route_simple(self, mock_recv, mock_send):
         self.app.route('/path1')(MagicMock())
         handler, match = self.app.match('/path1')
         assert handler
 
-    def test_route_params_1(self):
+    def test_route_params_1(self, mock_recv, mock_send):
         self.app.route('/path2/([^/]+)')(MagicMock())
         handler, match = self.app.match('/path2/foo')
         assert handler
         assert match.group(1) == 'foo'
 
-    def test_route_params_2(self):
+    def test_route_params_2(self, mock_recv, mock_send):
         self.app.route('/path2/([^/]+)')(MagicMock())
         handler, match = self.app.match('/path2/foo/bar')
         assert handler is None
 
-    def test_send_response(self):
+    def test_send_response(self, mock_recv, mock_send):
         sock = MagicMock()
         self.app.send_response(sock, 200, 'Okay', 'This is a test')
         assert sock.write.called
         assert sock.write.call_args_list[0][0][0] == b'HTTP/1.1 200 Okay\r\n'
         assert sock.write.call_args_list[-1][0][0] == b'This is a test'
 
-    def test_request_404(self):
-        with patch('noggin.compat.socket.mpsocket.recv') as recv, \
-                patch('noggin.compat.socket.mpsocket.send') as send:
-            recv.side_effect = (bytes([b]) for b in
-                                b'GET /\r\n\r\n')
-            client = noggin.compat.socket.mpsocket()
-            self.app._handle_client(client, '1.2.3.4.')
+    def test_request_404(self, mock_recv, mock_send):
+        mock_recv.side_effect = (bytes([b]) for b in
+                                 b'GET /\r\n\r\n')
+        client = noggin.compat.socket.mpsocket()
+        self.app._handle_client(client, '1.2.3.4.')
 
-            assert send.called
-            assert (send.call_args_list[0][0][0] ==
-                    b'HTTP/1.1 404 Not Found\r\n')
+        assert mock_send.called
+        assert (mock_send.call_args_list[0][0][0] ==
+                b'HTTP/1.1 404 Not Found\r\n')
 
-    def test_request_200(self):
+    def test_request_200(self, mock_recv, mock_send):
         @self.app.route('/')
         def handler(req):
             return 'This is a test'
 
-        with patch('noggin.compat.socket.mpsocket.recv') as recv, \
-                patch('noggin.compat.socket.mpsocket.send') as send:
-            recv.side_effect = (bytes([b]) for b in
-                                b'GET /\r\n\r\n')
-            client = noggin.compat.socket.mpsocket()
-            self.app._handle_client(client, '1.2.3.4.')
+        mock_recv.side_effect = (bytes([b]) for b in
+                                 b'GET /\r\n\r\n')
+        client = noggin.compat.socket.mpsocket()
+        self.app._handle_client(client, '1.2.3.4.')
 
-            assert send.called
-            assert (send.call_args_list[0][0][0] ==
-                    b'HTTP/1.1 200 Okay\r\n')
-            assert (send.call_args_list[-1][0][0] ==
-                    b'This is a test')
+        assert mock_send.called
+        assert (mock_send.call_args_list[0][0][0] ==
+                b'HTTP/1.1 200 Okay\r\n')
+        assert (mock_send.call_args_list[-1][0][0] ==
+                b'This is a test')
 
-    def test_custom_response(self):
+    def test_custom_response(self, mock_recv, mock_send):
         @self.app.route('/')
         def handler(req):
             return noggin.Response(200, 'Custom status',
                                    'This is a test',
                                    content_type='text/html')
 
-        with patch('noggin.compat.socket.mpsocket.recv') as recv, \
-                patch('noggin.compat.socket.mpsocket.send') as send:
-            recv.side_effect = (bytes([b]) for b in
-                                b'GET /\r\n\r\n')
-            client = noggin.compat.socket.mpsocket()
-            self.app._handle_client(client, '1.2.3.4.')
+        mock_recv.side_effect = (bytes([b]) for b in
+                                 b'GET /\r\n\r\n')
+        client = noggin.compat.socket.mpsocket()
+        self.app._handle_client(client, '1.2.3.4.')
 
-            assert send.called
-            assert (send.call_args_list[0][0][0] ==
-                    b'HTTP/1.1 200 Custom status\r\n')
-            assert (send.call_args_list[1][0][0] ==
-                    b'Content-type: text/html\r\n')
+        assert mock_send.called
+        assert (mock_send.call_args_list[0][0][0] ==
+                b'HTTP/1.1 200 Custom status\r\n')
+        assert (mock_send.call_args_list[1][0][0] ==
+                b'Content-type: text/html\r\n')
 
-    def test_read_simple(self):
+    def test_read_simple(self, mock_recv, mock_send):
         @self.app.route('/', methods=['PUT'])
         def handler(req):
             return req.content
 
-        def fake_recv_into(self, buf, buflen=0, flags=0):
-            c = self.recv(1)
-            if c is None:
-                return 0
+        mock_recv.side_effect = [
+            bytes([b]) for b in
+            b'PUT /\r\n'
+            b'Content-length: 15\r\n'
+            b'\r\n'
+            b'This is a test'] + [None]
+        client = noggin.compat.socket.mpsocket()
+        self.app._handle_client(client, ('1.2.3.4.', 1234))
 
-            buf[0] = ord(c)
-            return 1
+        assert mock_send.called
+        assert (mock_send.call_args_list[0][0][0] ==
+                b'HTTP/1.1 200 Okay\r\n')
+        assert (mock_send.call_args_list[-1][0][0] ==
+                b'This is a test')
 
-        with patch('noggin.compat.socket.mpsocket.recv') as recv, \
-                patch('noggin.compat.socket.mpsocket.send') as send, \
-                patch('noggin.compat.socket.mpsocket.recv_into',
-                      fake_recv_into):
-
-            recv.side_effect = [bytes([b]) for b in
-                                b'PUT /\r\n'
-                                b'Content-length: 15\r\n'
-                                b'\r\n'
-                                b'This is a test'] + [None]
-            client = noggin.compat.socket.mpsocket()
-            self.app._handle_client(client, ('1.2.3.4.', 1234))
-
-            assert send.called
-            assert (send.call_args_list[0][0][0] ==
-                    b'HTTP/1.1 200 Okay\r\n')
-            assert (send.call_args_list[-1][0][0] ==
-                    b'This is a test')
-
-    def test_read_chunked(self):
+    def test_read_chunked(self, mock_recv, mock_send):
         @self.app.route('/', methods=['PUT'])
         def handler(req):
             return req.content
 
-        def fake_recv_into(self, buf, buflen=0, flags=0):
-            c = self.recv(1)
-            if c is None:
-                return 0
+        mock_recv.side_effect = [
+            bytes([b]) for b in
+            b'PUT /\r\n'
+            b'Transfer-encoding: chunked\r\n'
+            b'Content-length: 15\r\n'
+            b'\r\n'
+            b'E\r\n'
+            b'This is a test'
+            b'\r\n'
+            b'0\r\n'
+            b'\r\n'] + [None]
+        client = noggin.compat.socket.mpsocket()
+        self.app._handle_client(client, ('1.2.3.4.', 1234))
 
-            buf[0] = ord(c)
-            return 1
-
-        with patch('noggin.compat.socket.mpsocket.recv') as recv, \
-                patch('noggin.compat.socket.mpsocket.send') as send, \
-                patch('noggin.compat.socket.mpsocket.recv_into',
-                      fake_recv_into):
-
-            recv.side_effect = [bytes([b]) for b in
-                                b'PUT /\r\n'
-                                b'Transfer-encoding: chunked\r\n'
-                                b'Content-length: 15\r\n'
-                                b'\r\n'
-                                b'E\r\n'
-                                b'This is a test'
-                                b'\r\n'
-                                b'0\r\n'
-                                b'\r\n'] + [None]
-            client = noggin.compat.socket.mpsocket()
-            self.app._handle_client(client, ('1.2.3.4.', 1234))
-
-            assert send.called
-            assert (send.call_args_list[0][0][0] ==
-                    b'HTTP/1.1 200 Okay\r\n')
-            assert (send.call_args_list[-1][0][0] ==
-                    b'This is a test')
+        assert mock_send.called
+        assert (mock_send.call_args_list[0][0][0] ==
+                b'HTTP/1.1 200 Okay\r\n')
+        assert (mock_send.call_args_list[-1][0][0] ==
+                b'This is a test')
