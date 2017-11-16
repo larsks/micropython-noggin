c79bcb964074225c300956a2d5c86524205bf742
Lars Kellogg-Stedman <lars@oddbit.com>
2017-11-16 18:11:03 -0500

    docstrings for tests

--- tests/test_noggin.py
+++ tests/test_noggin.py
@@ -20,26 +20,38 @@ class TestNoggin(TestCase):
     def setUp(self):
         self.app = noggin.Noggin()
 
-    def test_create_app(self, mock_recv, mock_send):
-        assert self.app
-
     def test_route_simple(self, mock_recv, mock_send):
+
+        '''Does a simple route work as expected?'''
+
         self.app.route('/path1')(MagicMock())
         handler, match = self.app.match('/path1')
         assert handler
 
     def test_route_params_1(self, mock_recv, mock_send):
+
+        '''Do routes with match groups work as expected?'''
+
         self.app.route('/path2/([^/]+)')(MagicMock())
         handler, match = self.app.match('/path2/foo')
         assert handler
         assert match.group(1) == 'foo'
 
     def test_route_params_2(self, mock_recv, mock_send):
+
+        '''Are routes correctly anchored so that we don't get
+        erroneous matches when the initial partial of our request uri
+        matches an existing route?'''
+
         self.app.route('/path2/([^/]+)')(MagicMock())
         handler, match = self.app.match('/path2/foo/bar')
         assert handler is None
 
     def test_send_response(self, mock_recv, mock_send):
+
+        '''Does calling send_response generate the expected
+        content?'''
+
         sock = MagicMock()
         self.app.send_response(sock, 200, 'Okay', 'This is a test')
         assert sock.write.called
@@ -47,6 +59,10 @@ class TestNoggin(TestCase):
         assert sock.write.call_args_list[-1][0][0] == b'This is a test'
 
     def test_request_404(self, mock_recv, mock_send):
+
+        '''If we make a request for a route that doesn't exist, do we
+        get a 404 error as expected?'''
+
         mock_recv.side_effect = (bytes([b]) for b in
                                  b'GET /\r\n\r\n')
         client = noggin.compat.socket.mpsocket()
@@ -57,6 +73,10 @@ class TestNoggin(TestCase):
                 b'HTTP/1.1 404 Not Found\r\n')
 
     def test_request_200(self, mock_recv, mock_send):
+
+        '''Can we successfully handle a request for an existing
+        route?'''
+
         @self.app.route('/')
         def handler(req):
             return 'This is a test'
@@ -91,6 +111,9 @@ class TestNoggin(TestCase):
                 b'Content-type: text/html\r\n')
 
     def test_read_simple(self, mock_recv, mock_send):
+
+        '''Can we correctly read the body of a request?'''
+
         @self.app.route('/', methods=['PUT'])
         def handler(req):
             return req.content
@@ -111,6 +134,10 @@ class TestNoggin(TestCase):
                 b'This is a test')
 
     def test_read_chunked(self, mock_recv, mock_send):
+
+        '''Can we correctly read a request using
+        Transfer-encoding: chunked?'''
+
         @self.app.route('/', methods=['PUT'])
         def handler(req):
             return req.content
